7f00848827ac6f6183302fe7fcc847ff699b7b68
ICX <haribabu81254@gmail.com>
2023-03-09 17:15:26 +0530

    new commit

---
+++ src/main/java/com/mycompany/jenkins_03_09_pipeline/Jenkins_03_09_pipeline.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jenkins_03_09_pipeline;
+
+/**
+ *
+ * @author ICX
+ */
+public class Jenkins_03_09_pipeline {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a=10;
+        int b=30;
+        System.out.println("Hello World!    "+a+b);
+    }
+}
